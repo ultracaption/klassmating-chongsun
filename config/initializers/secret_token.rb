ab1d95e74cd8b7f82cab6ab0520588c82a27a391
Jo Seunghyun <sky@izz.kr>
2012-04-09 22:03:57 +0900

    Create new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KlassmatingChongsun::Application.config.secret_token = '7f1a88952a178769795bf179fee1262a9b23954a8e10ab5d6c3fbc9320114c38439f637a5bcb5f680c6c6fbe7527ce544f746e3564afa5ed4fa3a8d8a4c2064f'
